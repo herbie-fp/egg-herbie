361443a42fe477ee07093b10c0db7debbcab5eb6
oliver <oflatt@gmail.com>
2019-11-21 13:32:16 -0700

    try deploy script instead of pages

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+    git checkout -b egg-herbie-deploy-$TRAVIS_OS_NAME
+    rm .gitignore
+    rm .travis.yml
+    git add --all
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+    git remote add origin-pages https://${GITHUB_TOKEN}@github.com/oflatt/egg-herbie > /dev/null 2>&1
+    git push --quiet --set-upstream origin-pages egg-herbie-deploy-$TRAVIS_OS_NAME
+}
+
+setup_git
+commit_website_files
+upload_files
