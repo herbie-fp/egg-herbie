2231db8be837e68f3a5b6087562f07c0f0886273
Pavel Panchekha <me@pavpanchekha.com>
2020-05-01 09:44:23 -0600

    Handle deployment by OS

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 
+TAG=generic
+if [ "_$1" = "_ubuntu-latest" ]; then
+    TAG=linux
+elif [ "_$1" = "_windows-latest" ]; then
+    TAG=windows
+elif [ "_$1" = "_macos-latest" ]; then
+    TAG=osx
+else
+    echo "Invalid OS tag! Cannot deploy!"
+    exit 4
+fi
+
 setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
-    git checkout -b egg-herbie-deploy-$TRAVIS_OS_NAME
+    git checkout -b egg-herbie-deploy-$TAG
     git add -u
     git add target/release/* -f
     git add .travis.yml
-    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    git commit --message "Travis build: $(git rev-parse HEAD)"
 }
 
 upload_files() {
     git remote add origin-pages https://${GITHUB_TOKEN}@github.com/oflatt/egg-herbie > /dev/null 2>&1
-    git fetch origin-pages egg-herbie-deploy-$TRAVIS_OS_NAME
-    git merge origin-pages/egg-herbie-deploy-$TRAVIS_OS_NAME -s ours
+    git fetch origin-pages egg-herbie-deploy-$TAG
+    git merge origin-pages/egg-herbie-deploy-$TAG
     git commit
-    git push --set-upstream origin-pages egg-herbie-deploy-$TRAVIS_OS_NAME
+    git push --set-upstream origin-pages egg-herbie-deploy-$TAG
 }
 
 setup_git
