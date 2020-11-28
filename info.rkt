ea4737a2145987a281a8d18e9446cd225f6f8f77
oliver <oflatt@gmail.com>
2020-11-27 18:47:28 -0700

    just add redirect

--- info.rkt
+++ info.rkt
@@ -1,9 +1,14 @@
 #lang info
 
-(define collection "egg-herbie")
+(define collection "egg-herbie-redirect")
 (define version "1.4")
 
 (define pkg-desc "Racket bindings for simplifying math expressions using egg")
+
+(define deps
+  '(("egg-herbie-windows" #:platform "win32\\x86_64" #:version "1.4")
+    ("egg-herbie-osx" #:platform "x86_64-macosx" #:version "1.4")
+    ("egg-herbie-linux" #:platform "x86_64-linux" #:version "1.4")))
 
 (define pkg-authors
   `("Oliver Flatt"))
