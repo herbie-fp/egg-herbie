adb89cc6a3df94cc3bc5d033be605dcf52ce6ca7
Pavel Panchekha <me@pavpanchekha.com>
2024-03-26 19:02:50 -0600

    Woops

--- info.rkt
+++ info.rkt
@@ -1,15 +1,15 @@
 #lang info
 
 (define collection "egg-herbie-redirect")
-(define version "1.6")
+(define version "2.0")
 
 (define pkg-desc "Racket bindings for simplifying math expressions using egg")
 
 (define deps
-  '(("egg-herbie-osx" #:platform "x86_64-macosx" #:version "1.6")
-    ("egg-herbie-windows" #:platform "win32\\x86_64" #:version "1.6")
-    ("egg-herbie-linux" #:platform "x86_64-linux" #:version "1.6")
-    ("egg-herbie-linux" #:platform "x86_64-linux-natipkg" #:version "1.6")))   ; Dockerfile
+  '(("egg-herbie-osx" #:platform "x86_64-macosx" #:version "2.0")
+    ("egg-herbie-windows" #:platform "win32\\x86_64" #:version "2.0")
+    ("egg-herbie-linux" #:platform "x86_64-linux" #:version "2.0")
+    ("egg-herbie-linux" #:platform "x86_64-linux-natipkg" #:version "2.0")))   ; Dockerfile
 
 (define pkg-authors
   `("Oliver Flatt"
